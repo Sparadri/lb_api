60f1e442778937865e4449aa4d4e4e1b5eaa55ca
Adrien de Villoutreys <adrien.de.villoutreys@gmail.com>
2016-12-30 12:39:00 +0100

    freed api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :verify_api_key
+  # before_action :verify_api_key
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
